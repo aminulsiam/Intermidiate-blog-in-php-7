9b27e7aea839f9666d4a13af469f554eb6386813
aminulsiam <aminulhossain90@gmail.com>
2018-06-13 00:19:44 +0600

    coding standert

--- adminbg/classes/user.php
+++ adminbg/classes/user.php
@@ -12,12 +12,14 @@
 
         private $db;
 
-        public function __construct() {
+        public function __construct() 
+        {
             $this->db = new DB;
         }
 
         // select all user for admin
-        public function selectUser() {
+        public function selectUser() 
+        {
             $query = "SELECT id,email,image,created_at,status FROM tbl_user ORDER BY id DESC";
             return $this->db->select($query);
         }
