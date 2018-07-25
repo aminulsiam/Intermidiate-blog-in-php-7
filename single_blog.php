c30b283a1d74be062ac72af61372b4012af09c2c
aminulsiam <36822365+aminulsiam@users.noreply.github.com>
2018-07-25 12:35:15 +0600

    Update single_blog.php

--- single_blog.php
+++ single_blog.php
@@ -23,33 +23,28 @@
 ?>
 
 <div class="grids">
-		<div class="grid box">
-			<div class="grid-header">
-				<h3 align="center"><?php echo $value['blog_title'];?> </h3>
-				<ul>
-				<li><span>Post By <strong style="font-size: 2vh;color: #ea4c89;font-weight: bold;"><?php echo $value['blog_author'];?></strong> on <?php echo $value['created_at'];?></span></li>
-				<li><a href="#">5 comments</a></li>
-				</ul>
-			</div>
-			<div class="grid-img-content">
-			  <img src="adminbg/<?php echo $value['image'];?>" alt="picture" style="width:40vh;height:30vh;"/>
-			   <span style="font-size: 0.7em;"><?php echo $value['image_caption']; ?></span>			
-				<p class="grid-header"><?php echo $value['blog_description'];?>
-					
-				</p>
-				<div class="clear"> </div>
-			</div>
-			
-			<div class="clear"></div>
-		</div>
-		
-	
-			<div class="clear"> </div>
+	<div class="grid box">
+	   <div class="grid-header">
+	      <h3 align="center"><?php echo $value['blog_title'];?> </h3>
+	        <ul>
+		  <li><span>Post By <strong style="font-size: 2vh;color: #ea4c89;font-weight: bold;"><?php echo $value['blog_author'];?></strong> on <?php echo $value['created_at'];?></span></li>
+		  <li><a href="#">5 comments</a></li>
+	        </ul>
+	    </div>
+	<div class="grid-img-content">
+	  <img src="adminbg/<?php echo $value['image'];?>" alt="picture" style="width:40vh;height:30vh;"/>
+	   <span style="font-size: 0.7em;"><?php echo $value['image_caption']; ?></span>			
+		<p class="grid-header">
+		   <?php echo $value['blog_description'];?>
+		</p>
+	   <div class="clear"></div>
 	</div>
-			<div class="clear"> </div>
-			<!--  pagination -->
-			
-					<div class="clear"> </div>
+	   <div class="clear"></div>
+	</div>
+	   <div class="clear"> </div>
+	</div>
+	   <div class="clear"> </div>			
+	   <div class="clear"> </div>
 
 
 
