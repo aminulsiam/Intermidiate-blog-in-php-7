c58d0cbae3c711557d85005f342d757b79f1c4c1
aminulsiam <aminulhossain90@gmail.com>
2018-07-29 02:17:42 +0600

    comment successfully done

--- single_blog.php
+++ single_blog.php
@@ -1,9 +1,6 @@
 <?php
 	require "classes/blog.php";
-<<<<<<< HEAD
 	require "classes/comment.php";
-=======
->>>>>>> 2d4f5f66d138d503287fe4dbe41a0207aae05c70
 
 	use Frontend\Blog\Blog;
 	use Frontend\Comment\Comment;
@@ -15,19 +12,15 @@
 		$title = $_GET['title'];
 		$posts = $blog->selectTitlewiseBlog($title);
 		$value = $posts->fetch(PDO::FETCH_ASSOC);
-<<<<<<< HEAD
 	}
 
 	if($_SERVER['REQUEST_METHOD']=='POST'){
 		$comment->saveComment($_POST,$title);
 	}
 	
-=======
-	}	
->>>>>>> 2d4f5f66d138d503287fe4dbe41a0207aae05c70
 ?>
+
 <div class="grids">
-<<<<<<< HEAD
 		<div class="grid box">
 			<div class="grid-header">
 				<h3 align="center"><?php echo $value['blog_title'];?> </h3>
@@ -72,8 +65,6 @@
 			<?php }else{?>
 				<h3 style="float: right;color:#ea4c89;margin-top: 20px;margin-right: 23%;font-weight: bold;">কমেন্ট করতে হলে আপনাকে লগিন করতে হবে ।</h3>
 			<?php }?>
-
-
 			</div>
 			<div class="clear"></div>
 
@@ -81,30 +72,11 @@
 		
 	
 			<div class="clear"> </div>
-=======
-	<div class="grid box">
-	   <div class="grid-header">
-	      <h3 align="center"><?php echo $value['blog_title'];?> </h3>
-	        <ul>
-		  <li><span>Post By <strong style="font-size: 2vh;color: #ea4c89;font-weight: bold;"><?php echo $value['blog_author'];?></strong> on <?php echo $value['created_at'];?></span></li>
-		  <li><a href="#">5 comments</a></li>
-	        </ul>
-	    </div>
-	<div class="grid-img-content">
-	  <img src="adminbg/<?php echo $value['image'];?>" alt="picture" style="width:40vh;height:30vh;"/>
-	   <span style="font-size: 0.7em;"><?php echo $value['image_caption']; ?></span>			
-		<p class="grid-header">
-		   <?php echo $value['blog_description'];?>
-		</p>
-	   <div class="clear"></div>
 	</div>
-	   <div class="clear"></div>
-	</div>
-	   <div class="clear"> </div>
->>>>>>> 2d4f5f66d138d503287fe4dbe41a0207aae05c70
-	</div>
-	   <div class="clear"> </div>			
-	   <div class="clear"> </div>
+			<div class="clear"> </div>
+			<!--  pagination -->
+			
+					<div class="clear"> </div>
 
 
 
