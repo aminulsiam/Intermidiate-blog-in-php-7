d30c88b6fe5a4f6c03036aa7c19a8f45664ed828
aminulsiam <aminulhossain90@gmail.com>
2018-09-01 19:57:16 +0600

    primary release

--- adminbg/add_category.php
+++ adminbg/add_category.php
@@ -1,45 +1,37 @@
 <?php
-	require_once 'classes/category.php';
+require_once 'classes/category.php';
 
-    use Category\Category;
+use Category\Category;
 
-	$category = new Category;
-	if($_SERVER['REQUEST_METHOD'] == 'POST'){
-		$msg = $category->saveCategory($_POST);
-	}
+$category = new Category;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $msg = $category->saveCategory($_POST);
+}
 ?>
- <!--header start-->
-        <?php include 'inc/header.php';?>
-    <!--header end-->
-    <!--sidebar start-->
-        <?php include 'inc/sidebar.php';?>
-    <!--sidebar end-->
+<!--header start-->
+<?php include 'inc/header.php'; ?>
+<!--header end-->
+<!--sidebar start-->
+<?php include 'inc/sidebar.php'; ?>
+<!--sidebar end-->
 <!--main content start-->
 <section id="main-content">
-	
-
-	<section class="wrapper">
-		<div class="form-w3layouts">
+    <section class="wrapper">
+        <div class="form-w3layouts">
             <!-- page start-->
-          
             <div class="row">
                 <div class="col-lg-12">
                     <section class="panel">
                         <header class="panel-heading">
                             <strong>Add Category</strong>
-                            <!-- <span class="tools pull-right">
-                                <a class="fa fa-chevron-down" href="javascript:;"></a>
-                                <a class="fa fa-cog" href="javascript:;"></a>
-                                <a class="fa fa-times" href="javascript:;"></a>
-                             </span> -->
                         </header>
                         <div class="">
                             <div class="form">
                                 <form action="" method="post" name="myform">
                                     <div class="form-group ">
                                         <label for="firstname" class="control-label col-lg-3 ">
-                                        	<span class="pull-right">Category name : </span>
-                                    	</label>
+                                            <span class="pull-right">Category name : </span>
+                                        </label>
                                         <div class="col-lg-6">
                                             <input type="text" class="form-control" onkeyup="checkAvailability()" required name="category" id="category" placeholder="Enter category name">
                                             <span id="category_availability"></span> 
@@ -53,9 +45,10 @@
                                 </form>
                             </div>
                         </div>
-                         <h3 class="text-center text-success">
-                         	<?php if(isset($msg)){  
-                         		echo '<script type="text/javascript">
+                        <h3 class="text-center text-success">
+                            <?php
+                            if (isset($msg)) {
+                                echo '<script type="text/javascript">
                          				jQuery(function() {
 									        swal({
 											  title: "Blog category created!!",
@@ -64,15 +57,16 @@
 											});
 									    });
                          			  </script> ';
-                         	 }?>
-                         </h3>
+                            }
+                            ?>
+                        </h3>
                     </section>
                 </div>
             </div>
             <!-- page end-->
         </div>
-</section>
+    </section>
 
-         <!-- footer -->
-            <?php include 'inc/footer.php';?>		  
-         <!-- / footer -->
+    <!-- footer -->
+<?php include 'inc/footer.php'; ?>		  
+    <!-- / footer -->
